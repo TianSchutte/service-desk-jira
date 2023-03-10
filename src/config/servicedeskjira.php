c9dae2d27219debb096fa4d69665a9c104f5ab41
Tian Schutte <tian@giantprocurement.guru>
2023-03-10 12:27:25 +0000

    re-added config

---
+++ src/config/servicedeskjira.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'base_url' => env('JIRA_SERVICE_DESK_BASE_URL', 'https://base.atlassian.net'),
+    'email' => env('JIRA_SERVICE_DESK_EMAIL', 'base@email.guru'),
+    'api_key' => env('JIRA_SERVICE_DESK_API_KEY','base'),
+    'project_id' => env('JIRA_SERVICE_DESK_PROJECT_ID', '6'),
+];
